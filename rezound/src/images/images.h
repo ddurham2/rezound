17a905250b101383d6911fa5382e0839f92995c0
Davy Durham <davydurham@gmail.com>
2002-05-20 15:24:44 +0000

    Changed some of the icons for the play controls

--- rezound/src/images/images.h
+++ rezound/src/images/images.h
@@ -11,20 +11,18 @@ extern const unsigned char new_gif[];
 extern const unsigned char notes_gif[];
 extern const unsigned char OffLED1_gif[];
 extern const unsigned char open_gif[];
-extern const unsigned char pause_off_gif[];
-extern const unsigned char pause_on_gif[];
-extern const unsigned char play_off_gif[];
-extern const unsigned char play_on_gif[];
-extern const unsigned char record_off_gif[];
-extern const unsigned char record_on_gif[];
+extern const unsigned char pause_gif[];
+extern const unsigned char play_all_looped_gif[];
+extern const unsigned char play_all_once_gif[];
+extern const unsigned char play_selection_looped_gif[];
+extern const unsigned char play_selection_once_gif[];
 extern const unsigned char RedLED1_gif[];
 extern const unsigned char RedMinus1_gif[];
 extern const unsigned char RedX1_gif[];
 extern const unsigned char revert_gif[];
 extern const unsigned char saveas_gif[];
 extern const unsigned char save_gif[];
-extern const unsigned char stop_off_gif[];
-extern const unsigned char stop_on_gif[];
+extern const unsigned char stop_gif[];
 extern const unsigned char YellowLED1_gif[];
 
 #endif
