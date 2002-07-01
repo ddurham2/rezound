1ef6d5dc38755c647aebab58570c67607075b13e
Davy Durham <davydurham@gmail.com>
2002-07-01 15:56:29 +0000

    Added this script which creates a standalone binary pacakge for x86 linux.. Perhaps I will be expanded for other platforms later

---
+++ rezound/make_standalone.sh
@@ -0,0 +1,25 @@
+
+if [ -z $1 ]
+then
+	echo usage: $0 release_name
+	exit 1
+fi
+
+#FOX: `configure --with-opengl=no --disable-jpeg --disable-png --disable-tiff --disable-zlib --disable-bz2lib` and `make install`
+#AUDIOFILE: apply any of my patches to audiofile and `configure` and `make install`
+#cd to source directory
+
+
+dir=rezound-bin-x86-linux-$1
+
+mkdir $dir
+configure --prefix=`pwd`/$dir --enable-standalone --with-audiofile-include=/usr/local/include --with-audiofile-path=/usr/local/lib --with-FOX-include=/usr/local/include --with-FOX-path=/usr/local/lib
+
+make clean
+make
+make install-strip
+
+tar -cvzf ~/$dir.tar.gz $dir
+
+rm -rf $dir
+
