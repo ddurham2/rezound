0637f328259aee86a452a67561a7cbe648995c17
Davy Durham <davydurham@gmail.com>
2002-07-28 04:40:20 +0000

    Changed the 8192 bytes of prebuffering for OSS recording to 4096 because the SB16 driver didn't didn't like buffers that big

--- rezound/src/backend/COSSSoundRecorder.cpp
+++ rezound/src/backend/COSSSoundRecorder.cpp
@@ -49,8 +49,8 @@
 
 // ??? as the sample rate is lower these need to be lower so that onData is called more often and the view meters on the record dialog don't seem to lag
 
-#define BUFFER_SIZE_BYTES 8192						// buffer size in bytes
-#define BUFFER_SIZE_BYTES_LOG2 13					// log2(BUFFER_SIZE_BYTES) -- that is 2^this is BUFFER_SIZE_BYTES
+#define BUFFER_SIZE_BYTES 4096						// buffer size in bytes
+#define BUFFER_SIZE_BYTES_LOG2 12					// log2(BUFFER_SIZE_BYTES) -- that is 2^this is BUFFER_SIZE_BYTES
 
 
 COSSSoundRecorder::COSSSoundRecorder() :
