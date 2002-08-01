57c3cbdfd8e4d9b4e5fa9fd258f16c162e777bd6
Davy Durham <davydurham@gmail.com>
2002-08-01 07:12:16 +0000

    Made many enhancements to TSoundStretcher.h including a method to get the current source position.  And supporting fractional buffer sizes (actually the src should probably contain at least the ceil() of the srcLength)

--- rezound/src/backend/TSoundStretcher.h
+++ rezound/src/backend/TSoundStretcher.h
@@ -34,73 +34,171 @@ public:
 	 * srcOffset is the first sample that this class will look at
   	 * srcLength is the length of samples beyond the srcOffset that should be read
 	 * toLength is the amount of times that getSample() will be called
+	 * frameSize can be passed if the src is actually interlaced data of more than 1 channel of audio
+	 * frameOffset should be passed when frameSize is given other than one to say which channel in the interlaced data should be processed
+	 * paddedForInterpolation can be passed as true to tell TSoundStretcher not to worry about overrunning the src index since the caller has padded at the end for possible sample interpolation (currently only 1 extra sample frame would ever be needed)
 	 */
-	TSoundStretcher(const src_type &_src,const sample_pos_t _srcOffset,const sample_pos_t _srcLength,const sample_pos_t _toLength) :
+	TSoundStretcher(const src_type &_src,const sample_fpos_t _srcOffset,const sample_fpos_t _srcLength,const sample_fpos_t _toLength,unsigned _frameSize=1,unsigned _frameOffset=0,bool paddedEndForInterpolation=false) :
 		src(_src),
 		srcOffset(_srcOffset),
-		srcLength(_srcLength>0 ? _srcLength-1 : _srcLength),
+		srcLength((_srcLength>0 && !paddedEndForInterpolation) ? _srcLength-1 : _srcLength),
 		toLength(_toLength),
-		step(_srcLength/_toLength),
+		frameSize(_frameSize),
+		frameOffset(_frameOffset),
+		step((int)floor(_srcLength/_toLength)),
 
 		pos(0)
 	{
-		if(_srcLength==_toLength)
-		{
-			pos=srcOffset;
-			_getSample= &TSoundStretcher<src_type>::copy_GetSample;
+		if(frameSize==0)
+			throw(runtime_error(string(__func__)+" -- frameSize is 0"));
+		if(frameOffset>=frameSize)
+			throw(runtime_error(string(__func__)+" -- frameOffset is >= frameSize: "+istring(frameOffset)+">="+istring(frameSize)));
+
+		// determine which actual implementation we can use based on the given parameters
+		// use less expensive scaling methods when possible, else use 2-point sample interpolation
+		if(srcLength==floor(srcLength) && toLength==floor(toLength))
+		{ // both srcLength and toLength are integers
+			if(srcOffset==floor(srcOffset) && _srcLength==_toLength)
+			{ // srcOffset is an integer and the scaling is 1.0
+				pos=(long)floor(srcOffset);
+				if(frameSize==1)
+					_getSample= &TSoundStretcher<src_type>::copy_frameSize1_GetSample;
+				else
+					_getSample= &TSoundStretcher<src_type>::copy_frameSizeN_GetSample;
+				_getCurrentSrcPosition= &TSoundStretcher<src_type>::copy_GetCurrentSrcPosition;
+			}
+			else if(srcOffset==floor(srcOffset) && ((long)floor(_srcLength)%(long)floor(_toLength))==0)
+			{ // srcOffset is an integer and srcLength is an integral multiple of toLength
+				pos=(long)floor(srcOffset);
+				if(frameSize==1)
+					_getSample= &TSoundStretcher<src_type>::step_frameSize1_GetSample;
+				else
+					_getSample= &TSoundStretcher<src_type>::step_frameSizeN_GetSample;
+				_getCurrentSrcPosition= &TSoundStretcher<src_type>::step_GetCurrentSrcPosition;
+			}
+			else if(_srcLength<=1)
+			{
+				if(frameSize==1)
+					_getSample= &TSoundStretcher<src_type>::scale_frameSize1_GetSample;
+				else
+					_getSample= &TSoundStretcher<src_type>::scale_frameSizeN_GetSample;
+				_getCurrentSrcPosition= &TSoundStretcher<src_type>::scale_GetCurrentSrcPosition;
+			}
+			else
+			{
+				if(frameSize==1)
+					_getSample= &TSoundStretcher<src_type>::linearInterpolate_frameSize1_GetSample;
+				else
+					_getSample= &TSoundStretcher<src_type>::linearInterpolate_frameSizeN_GetSample;
+				_getCurrentSrcPosition= &TSoundStretcher<src_type>::linearInterpolate_GetCurrentSrcPosition;
+			}
 		}
-		else if((_srcLength%_toLength)==0)
-		{
-			pos=srcOffset;
-			_getSample= &TSoundStretcher<src_type>::step_GetSample;
-		}
-		else if(_srcLength<=1)
-			_getSample= &TSoundStretcher<src_type>::scale_GetSample;
 		else
-			_getSample= &TSoundStretcher<src_type>::linearInterpolate_GetSample;
+		{
+			if(frameSize==1)
+				_getSample= &TSoundStretcher<src_type>::linearInterpolate_frameSize1_GetSample;
+			else
+				_getSample= &TSoundStretcher<src_type>::linearInterpolate_frameSizeN_GetSample;
+			_getCurrentSrcPosition= &TSoundStretcher<src_type>::linearInterpolate_GetCurrentSrcPosition;
+		}
 	}
 
 	const sample_t getSample() { return (this->*_getSample)(); }
 
+	// useful for the next time you create a TSoundStretcher if TSoundStretcher is being used multiple times for chunk divided data
+	const sample_fpos_t getCurrentSrcPosition() const { return (this->*_getCurrentSrcPosition)(); }
 
 private:
 	const src_type src;
-	const sample_pos_t srcOffset;
+	const sample_fpos_t srcOffset;
 	const sample_fpos_t srcLength;
 	const sample_fpos_t toLength;
+	const sample_pos_t frameSize;
+	const sample_pos_t frameOffset;
 	const sample_pos_t step;
 
 	sample_pos_t pos;
 
-	const sample_t copy_GetSample()
+	// --- copy ----------------------------
+	const sample_t copy_frameSize1_GetSample()
 	{
 		return(src[pos++]);
 	}
+	const sample_t copy_frameSizeN_GetSample()
+	{
+		return(src[(pos++)*frameSize+frameOffset]);
+	}
+	const sample_fpos_t copy_GetCurrentSrcPosition() const
+	{
+		return pos;
+	}
 
-	const sample_t step_GetSample()
+
+	// --- step ----------------------------
+	const sample_t step_frameSize1_GetSample()
 	{
 		const sample_t s=src[pos];
 		pos+=step;
 		return(s);
 	}
+	const sample_t step_frameSizeN_GetSample()
+	{
+		const sample_t s=src[pos*frameSize+frameOffset];
+		pos+=step;
+		return(s);
+	}
+	const sample_fpos_t step_GetCurrentSrcPosition() const
+	{
+		return pos;
+	}
 
-	const sample_t scale_GetSample()
+
+	// --- scale ---------------------------
+	const sample_t scale_frameSize1_GetSample()
 	{ // only used for border condition cases
 		return(src[(sample_pos_t)(((sample_fpos_t)(pos++)/toLength*(srcLength+1.0))+srcOffset)]);
 	}
-
-	const sample_t linearInterpolate_GetSample()
+	const sample_t scale_frameSizeN_GetSample()
+	{ // only used for border condition cases
+		return(src[((sample_pos_t)(((sample_fpos_t)(pos++)/toLength*(srcLength+1.0))+srcOffset))*frameSize+frameOffset]);
+	}
+	const sample_fpos_t scale_GetCurrentSrcPosition() const
 	{
-		const sample_fpos_t fPos=((sample_fpos_t)(pos++))/toLength*srcLength;
+		return ((sample_fpos_t)(pos)/toLength*(srcLength+1.0))+srcOffset;
+	}
+
+
+	// --- linearInterpolate ---------------
+	const sample_t linearInterpolate_frameSize1_GetSample()
+	{
+		const sample_fpos_t fPos=(((sample_fpos_t)(pos++))/toLength*(srcLength))+srcOffset;
 		const sample_pos_t iPos=(sample_pos_t)fPos;
 		const float p2=fPos-iPos;
 		const float p1=1.0-p2;
-		return((sample_t)(p1*src[srcOffset+iPos]+p2*src[srcOffset+iPos+1]));
+		return((sample_t)(p1*src[iPos]+p2*src[iPos+1]));
 	}
+	const sample_t linearInterpolate_frameSizeN_GetSample()
+	{
+		const sample_fpos_t fPos=(((sample_fpos_t)(pos++))/toLength*(srcLength))+srcOffset;
+		const sample_pos_t iPos=(sample_pos_t)fPos;
+		const float p2=fPos-iPos;
+		const float p1=1.0-p2;
+		const sample_pos_t pos=iPos*frameSize+frameOffset;
+		return((sample_t)(p1*src[pos]+p2*src[pos+frameSize]));
+	}
+	const sample_fpos_t linearInterpolate_GetCurrentSrcPosition() const
+	{
+		return (((sample_fpos_t)pos)/toLength*(srcLength))+srcOffset;
+	}
+
+
+
 
 	typedef const sample_t (TSoundStretcher<src_type>::*getSample_t)();
 	getSample_t _getSample;
 
+	typedef const sample_fpos_t (TSoundStretcher<src_type>::*getCurrentSrcPosition_t)() const;
+	getCurrentSrcPosition_t _getCurrentSrcPosition;
 };
 
 #endif
