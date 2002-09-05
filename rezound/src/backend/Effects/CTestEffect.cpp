3764bcf65bb68dd368f352b3db7d7a71f898e3fc
Davy Durham <davydurham@gmail.com>
2002-09-05 01:38:22 +0000

    Played some more with the test effect and different filter kernels

--- rezound/src/backend/Effects/CTestEffect.cpp
+++ rezound/src/backend/Effects/CTestEffect.cpp
@@ -50,39 +50,67 @@ bool CTestEffect::doActionSizeSafe(CActionSound &actionSound,bool prepareForUndo
 	#include "/home/ddurham/impulse/impulse.h"
 	
 	const sample_pos_t M=IMPULSE_LENGTH; // length of filter kernel
-	TAutoBuffer<float> input_kernel(M);
+	TAutoBuffer<float> filter_kernel(M);
 	for(sample_pos_t t=0;t<M;t++)
-		input_kernel[t]=impulse[t];
+		filter_kernel[t]=impulse[t];
 
-#else
+#elif 0
 	// create the time-domain filter kernel
 	const float freq=(float)100/(float)actionSound.sound->getSampleRate(); // cutoff frequency of this low-pass filter (fraction of the sample rate 0..0.5)
 	const sample_pos_t M=200; // length of filter kernel
-	float input_kernel[M];
+	float filter_kernel[M];
 	for(sample_pos_t i=0;i<M;i++) 
 	{
 		if(i!=M/2)
-			input_kernel[i]=sin(2*M_PI*freq*(i-M/2))/(i-M/2)*(0.42-0.5*cos(2*M_PI*i/M)+0.08*cos(4*M_PI*i/M));
+			filter_kernel[i]=sin(2*M_PI*freq*(i-M/2))/(i-M/2);
 		else // avoid div by 0
-			input_kernel[i]=2*M_PI*freq;
+			filter_kernel[i]=2*M_PI*freq;
+	}
+#elif 0
+	
+	// create the time-domain filter kernel
+	const sample_pos_t M=1000; // length of filter kernel
+	float filter_kernel[M];
+	for(sample_pos_t i=0;i<M;i++) 
+	{
+		filter_kernel[i]=(sin((i+10)/50.0)/3.0)*cos((i-13)/10.0)/3;
+		//filter_kernel[i]=(sin(i/50.0)/3.0)*(cos(i/10.0)/(i/(M/10.0)+1));
+		//filter_kernel[i]=sin(i/50.0)*cos(i/10.0)/(cos((float)i/M*M_PI)+2.0);
+	}
+#else
+	
+	// create the time-domain filter kernel
+	const sample_pos_t M=200; // length of filter kernel
+	float filter_kernel[M];
+	for(sample_pos_t i=0;i<M;i++) 
+	{
+		filter_kernel[i]=pow((float)(i)/M,2)+sin(i/10);
 	}
 #endif
 
+	// apply the blackman window to the kernel
+	for(sample_pos_t i=0;i<M;i++) 
+		filter_kernel[i]*=(0.42-0.5*cos(2*M_PI*i/M)+0.08*cos(4*M_PI*i/M));
+
+
+#if 1
 	// normalize the time-domain filter kernel
 	double sum=0;
 	for(sample_pos_t i=0;i<M;i++)
-		sum+=input_kernel[i];
+		sum+=filter_kernel[i];
 	for(sample_pos_t i=0;i<M;i++)
-		input_kernel[i]/=sum;
+		filter_kernel[i]/=sum;
+#endif
 
-	/*
+#if 0
 	// convert a time-domain low-pass filter kernel to a high-pass
 	for(sample_pos_t i=0;i<M;i++) 
-		input_kernel[i]=-input_kernel[i];
-	input_kernel[M/2]+=1.0;
-	*/
+		filter_kernel[i]=-filter_kernel[i];
+	filter_kernel[M/2]+=1.0;
+#endif
 
-	TFFTConvolver<sample_t,float> c(input_kernel,M);
+#ifdef HAVE_LIBRFFTW
+	TFFTConvolver<sample_t,float> c(filter_kernel,M);
 
 	for(unsigned i=0;i<actionSound.sound->getChannelCount();i++)
 	{
@@ -90,7 +118,7 @@ bool CTestEffect::doActionSizeSafe(CActionSound &actionSound,bool prepareForUndo
 		{
 			CRezPoolAccesser dest=actionSound.sound->getAudio(i);
 			const CRezPoolAccesser src=prepareForUndo ? actionSound.sound->getTempAudio(tempAudioPoolKey,i) : actionSound.sound->getAudio(i);
-			sample_pos_t srcOffset=prepareForUndo ? start : 0;
+			sample_pos_t srcOffset=prepareForUndo ? 0 : start;
 
 			c.reset();
 
@@ -128,6 +156,32 @@ bool CTestEffect::doActionSizeSafe(CActionSound &actionSound,bool prepareForUndo
 				actionSound.sound->invalidatePeakData(i,actionSound.start,actionSound.stop);
 		}
 	}
+#else
+	for(unsigned i=0;i<actionSound.sound->getChannelCount();i++)
+	{
+		if(actionSound.doChannel[i])
+		{
+			CRezPoolAccesser dest=actionSound.sound->getAudio(i);
+			const CRezPoolAccesser src=prepareForUndo ? actionSound.sound->getTempAudio(tempAudioPoolKey,i) : actionSound.sound->getAudio(i);
+			sample_pos_t srcOffset=prepareForUndo ? start : 0;
+
+			TSimpleConvolver<mix_sample_t,float> convolver(filter_kernel,M);
+
+			BEGIN_PROGRESS_BAR("Filtering -- Channel "+istring(i),start,stop); 
+
+			for(sample_pos_t t=start;t<=stop;t++)
+			{
+				dest[t]=ClipSample(convolver.processSample(src[t-srcOffset]));
+				UPDATE_PROGRESS_BAR(t);
+			}
+
+			END_PROGRESS_BAR();
+
+			if(!prepareForUndo)
+				actionSound.sound->invalidatePeakData(i,actionSound.start,actionSound.stop);
+		}
+	}
+#endif
 
 	return(true);
 }
