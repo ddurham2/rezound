be4d8b646cfbfcde6a214dca6c369bd0002eb5ba
Davy Durham <davydurham@gmail.com>
2003-01-26 07:55:08 +0000

    Added small version of the play icons and a script that created them

---
+++ rezound/src/images/make_menu_thumbs.sh
@@ -0,0 +1,9 @@
+
+
+for i in jump_to_beginning.gif jump_to_next_q.gif jump_to_previous_q.gif jump_to_selection.gif pause.gif play_all_looped.gif play_all_once.gif play_selection_looped.gif play_selection_once.gif record.gif small_record.gif stop.gif
+do
+	echo $i
+	convert -scale 14 $i small_$i
+done
+
+
