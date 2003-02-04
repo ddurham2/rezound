3cc8da79e21972b3e07f2156062aa2a7327ac573
Davy Durham <davydurham@gmail.com>
2003-02-04 19:02:49 +0000

    Added --enable-jack flag to the configure script which defines ENABLE_JACK or not

--- rezound/config/acconfig.h
+++ rezound/config/acconfig.h
@@ -21,8 +21,9 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  */
 
-#undef ENABLE_PORTAUDIO
 #undef ENABLE_OSS
+#undef ENABLE_PORTAUDIO
+#undef ENABLE_JACK
 
 
 #undef HAVE_LIBOGG
