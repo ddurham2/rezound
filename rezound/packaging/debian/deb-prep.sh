f34b8a57e2bfbfc8d129eda0e10d83d8eb371f37
lergnom <lergnom@users.sourceforge.net>
2003-02-10 14:20:54 +0000

    This is pretty much the extent of wha this file will become, I may decide to make it a little better later.

--- rezound/packaging/debian/deb-prep.sh
+++ rezound/packaging/debian/deb-prep.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
-#
-# $Id$
+# $Id deb-prep.sh,v 1.1 2003/02/10 14:11:35 lergnom Exp $
 # Anthony Ventimiglia
 #
 # This script is used to prepare the Rezound cvs repository for debianization.
 # It should be run from the top level directory of the cleanly checked out 
 # CVS repository.
 #
+# It's quick and dirty.
+#
 # For more info read packaging/debian/README.cvs
 
 ./bootstrap
 ./configure
+make dist
