09598e2c8e8a87fd2026329654afa82e08509a20
lergnom <lergnom@users.sourceforge.net>
2003-02-12 16:40:57 +0000

    Adding debian/ to the make dist tarball created, with all the cvs versions of the debian control files contained whithin.

--- rezound/packaging/debian/deb-prep.sh
+++ rezound/packaging/debian/deb-prep.sh
@@ -16,6 +16,15 @@ sed -e 's/\(AUTOHEADER="autoheader\)/\12.13/g' bootstrap.orig |\
     sed -e 's/\(ACLOCAL="aclocal\)/\1-1.5/g' |\
     sed -e 's/\(AUTOMAKE="automake\)/\1-1.5/g' > bootstrap
 
+
+mkdir debian ;
+DEB_FILES='changelog control copyright'
+for file in $DEB_FILE; do
+    cp packaging/debian/$file debian
+done
+cp Makefile.am Makefile.am.orig
+sed -s 's/\(EXTRA_DIST=\)/\1debian/g' Makefile.am.orig > Makefile.am
+
 ./bootstrap ;
 ./configure ;
 
@@ -24,9 +33,10 @@ make dist ;
 BUILD_DIR='packaging/debian/build'
 
 mv bootstrap.orig bootstrap
+mv Makefile.am.orig Makefile.am
+rm -rf debian
+
 mkdir $BUILD_DIR
 mv rezound*.tar.gz $BUILD_DIR
 cd $BUILD_DIR
 tar zxvf rezound*.tar.gz
-
-
