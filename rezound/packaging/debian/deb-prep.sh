cb9d8c9f6f973ed6aa4397a1443809aaff5de77c
lergnom <lergnom@users.sourceforge.net>
2003-02-12 18:22:39 +0000

    Ok I should be ready to test out the debianization. Just checking in before the first run

--- rezound/packaging/debian/deb-prep.sh
+++ rezound/packaging/debian/deb-prep.sh
@@ -9,6 +9,13 @@
 #
 # For more info read packaging/debian/README.cvs
 
+
+mkdir -v debian ;
+DEB_FILES='changelog control copyright rules README.Debian dirs rezound.1 menu postinst postrm preinst prerm'
+for file in $DEB_FILES; do
+    cp -v packaging/debian/$file debian/
+done
+
 cp bootstrap bootstrap.orig
 # The new autoheader doesn't need -l and causes an error
 sed -e 's/\(AUTOHEADER="autoheader\)/\12.13/g' bootstrap.orig |\
@@ -17,26 +24,22 @@ sed -e 's/\(AUTOHEADER="autoheader\)/\12.13/g' bootstrap.orig |\
     sed -e 's/\(AUTOMAKE="automake\)/\1-1.5/g' > bootstrap
 
 
-mkdir debian ;
-DEB_FILES='changelog control copyright'
-for file in $DEB_FILE; do
-    cp packaging/debian/$file debian
-done
-cp Makefile.am Makefile.am.orig
-sed -s 's/\(EXTRA_DIST=\)/\1debian/g' Makefile.am.orig > Makefile.am
 
+
+cp Makefile.am Makefile.am.orig
+sed -e 's/\(EXTRA_DIST=\)/\1debian/g' Makefile.am.orig > Makefile.am
+ 
 ./bootstrap ;
 ./configure ;
 
 make dist ;
 
+mv -v bootstrap.orig bootstrap
+mv -v Makefile.am.orig Makefile.am
+rm -rvf debian
+
 BUILD_DIR='packaging/debian/build'
-
-mv bootstrap.orig bootstrap
-mv Makefile.am.orig Makefile.am
-rm -rf debian
-
-mkdir $BUILD_DIR
-mv rezound*.tar.gz $BUILD_DIR
+mkdir -v $BUILD_DIR
+mv -v rezound*.tar.gz $BUILD_DIR
 cd $BUILD_DIR
-tar zxvf rezound*.tar.gz
+tar zxvf rezound*.tar.gz;
