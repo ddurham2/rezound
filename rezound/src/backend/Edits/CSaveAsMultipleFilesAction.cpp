f0f43f5c94fb7f0c23318e35f1ac1c3db4ea013d
Davy Durham <davydurham@gmail.com>
2003-03-21 06:30:06 +0000

    fixed the multi-line string literal

--- rezound/src/backend/Edits/CSaveAsMultipleFilesAction.cpp
+++ rezound/src/backend/Edits/CSaveAsMultipleFilesAction.cpp
@@ -213,25 +213,25 @@ bool CSaveAsMultipleFilesAction::doesWarrantSaving() const
 
 const string CSaveAsMultipleFilesAction::getExplaination()
 {
-	return "
-To save a large file (or just a selection from it) as several smaller segments you can create cues that define the segments and then click on \"Save As Multiple Files\" under the \"File\" menu.
-
-In general, cues can be named '(' and ')' to define the beginning and end of each segment to be saved.
-However, a ')' cue (closing a prior '(' cue) can be ommitted if a segment is to end at the beginning of the next segment.
-The very last ')' cue can also be ommitted if the last defined segment is to end at the end of the original audio file (or the end of the selection).
-Furthermore, the '(' cue can optionally be named '(xyz' if 'xyz' is to be included in the segment's filename.
-
-There are several parameters in the dialog that is displayed after selecting \"Save As Multiple Files\" under the \"File\" menu.
-The \"Save to Directory\" parameter is the directory to place each segment file into.
-The \"Filename Prefix\" will be placed before the optional 'xyz' from the '(xyz' cue name.
-The \"Filename Suffix\" will be placed after of the optional 'xyz' from the '(xyz' cue name.
-The \"Format\" parameter in specifies what audio format the segments should be saved as.
-After a segment's filename is formed by putting together, [directory]/[prefix][xyz][suffix].[format extension], if it contains '#' then the '#' will be replaced with a number based on the order that the segments are defined.
-    For instance: \"/home/john/sounds/track #.wav\" will be changed to \"/home/john/sounds/track 1.wav\" for the first segment that is saved, and all the subsequent segments will contain an increasing number.
-    Thus, you should use a '#' in either the save to directory, filename prefix, xyz, or the filename suffix to create unique filenames when saving the segments.
-The \"Segment Number Start\" parameter can be changed from '1' to start the '#' substitutions at something different.
-The \"Open Saved Segments\" can be selected simply if you want to open the segments after they have been saved.
-The \"Applies to\" parameter indicates if the action should regard only the current selection or the entire file.
+	return "\n\
+To save a large file (or just a selection from it) as several smaller segments you can create cues that define the segments and then click on \"Save As Multiple Files\" under the \"File\" menu.\n\
+\n\
+In general, cues can be named '(' and ')' to define the beginning and end of each segment to be saved.\n\
+However, a ')' cue (closing a prior '(' cue) can be ommitted if a segment is to end at the beginning of the next segment.\n\
+The very last ')' cue can also be ommitted if the last defined segment is to end at the end of the original audio file (or the end of the selection).\n\
+Furthermore, the '(' cue can optionally be named '(xyz' if 'xyz' is to be included in the segment's filename.\n\
+\n\
+There are several parameters in the dialog that is displayed after selecting \"Save As Multiple Files\" under the \"File\" menu.\n\
+The \"Save to Directory\" parameter is the directory to place each segment file into.\n\
+The \"Filename Prefix\" will be placed before the optional 'xyz' from the '(xyz' cue name.\n\
+The \"Filename Suffix\" will be placed after of the optional 'xyz' from the '(xyz' cue name.\n\
+The \"Format\" parameter in specifies what audio format the segments should be saved as.\n\
+After a segment's filename is formed by putting together, [directory]/[prefix][xyz][suffix].[format extension], if it contains '#' then the '#' will be replaced with a number based on the order that the segments are defined.\n\
+    For instance: \"/home/john/sounds/track #.wav\" will be changed to \"/home/john/sounds/track 1.wav\" for the first segment that is saved, and all the subsequent segments will contain an increasing number.\n\
+    Thus, you should use a '#' in either the save to directory, filename prefix, xyz, or the filename suffix to create unique filenames when saving the segments.\n\
+The \"Segment Number Start\" parameter can be changed from '1' to start the '#' substitutions at something different.\n\
+The \"Open Saved Segments\" can be selected simply if you want to open the segments after they have been saved.\n\
+The \"Applies to\" parameter indicates if the action should regard only the current selection or the entire file.\n\
 ";
 }
 
