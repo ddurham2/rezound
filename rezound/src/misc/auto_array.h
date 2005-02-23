9e6a6640b15d0e9f5174cc47fe4c82471639ff1a
Davy Durham <davydurham@gmail.com>
2005-02-23 08:09:47 +0000

    added an auto_array<> template class that sort of works like auto_ptr, but it is an array of pointers (which get deleted on destruction)

---
+++ rezound/src/misc/auto_array.h
@@ -0,0 +1,55 @@
+#ifndef __auto_array_H__
+#define __auto_array_H__
+
+// like auto_ptr, is an array of pointers of the type that will get freed at destruction
+#include <vector>
+
+namespace std {
+
+template<typename T> class auto_array : public vector<T *>
+{
+public:
+	auto_array(unsigned n_elements)
+	{
+		for(unsigned t=0;t<n_elements;t++)
+			push_back((T *)NULL);
+	}
+
+	virtual ~auto_array()
+	{
+		for(unsigned t=0;t<this->size();t++)
+			delete this->operator[](t);
+	}
+
+};
+
+/*
+template<typename T> class auto_array
+{
+public:
+	auto_array(unsigned n_elements)
+	{
+		for(unsigned t=0;t<n_elements;t++)
+			v.push_back(NULL);
+	}
+
+	virtual ~auto_array()
+	{
+		for(unsigned t=0;t<v.size();t++)
+			delete v[t];
+	}
+
+	T * operator[](unsigned index) const 
+	{ 
+		return v[index]; 
+	}
+
+private:
+	vector<T *> v;
+};
+*/
+
+};
+
+
+#endif
