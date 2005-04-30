86fda175e0e9106eb7f4b2853c21403e3b404e3f
Davy Durham <davydurham@gmail.com>
2005-04-30 22:01:07 +0000

    needed for fox-1.5

--- rezound/src/frontend_fox/fox_compat.h
+++ rezound/src/frontend_fox/fox_compat.h
@@ -18,6 +18,9 @@
 #elif defined(HAVE_LIBFOX_1_4)
 	#include <fox-1.4/fx.h>
 	#include <fox-1.4/fxkeys.h>
+#elif defined(HAVE_LIBFOX_1_5)
+	#include <fox-1.5/fx.h>
+	#include <fox-1.5/fxkeys.h>
 #else
 	#error no HAVE_LIBFOX defined
 #endif
