4e6b6ffe4a7d20f312950e7d2deba0ebc25f74ba
Davy Durham <davydurham@gmail.com>
2017-03-04 05:55:28 +0000

    eliminated boost dependency

--- rezound/src/misc/CAtomicCounter.h
+++ rezound/src/misc/CAtomicCounter.h
@@ -22,97 +22,29 @@
 
 #include "../../config/common.h"
 
-#if defined(__linux) && defined(__i386__) && ((__GNUC__ * 100) + __GNUC_MINOR__) < 401
-
-	// using linux's exposed atomic operations on Intel-32bit
-	#define LINUX_ATOMIX
-	#include <asm/atomic.h>
-
-#elif defined(__clang__) || defined(__GNUC__) && ((__GNUC__ * 100) + __GNUC_MINOR__) >= 405 && defined(__GXX_EXPERIMENTAL_CXX0X__)/*for now detect --std=c++0x.. will be removed later*/
-
-	// gcc-4.5 uses the atomic header (c++v0)
-	#define STL_ATOMIX
-	#include <atomic>
-
-#elif defined(__GNUC__) && ((__GNUC__ * 100) + __GNUC_MINOR__) >= 404 && defined(__GXX_EXPERIMENTAL_CXX0X__)/*for now detect --std=c++0x.. will be removed later*/
-
-	// gcc-4.4 introduced atomic operations into the STL (c++v0)
-	#define STL_ATOMIX
-	#include <cstdatomic>
-#else
-
-	// could use this only and nix the above three implementations.. need look into the cost tho
-	#define BOOST_ATOMIX
-	#include <boost/detail/atomic_count.hpp>
-
-#endif
+#include <atomic>
 
 class CAtomicCounter {
 public:
 	CAtomicCounter()
-#if defined(BOOST_ATOMIX)
-		: counter(0)
-#endif
 	{
-#if defined(LINUX_ATOMIX)
-		atomic_set(&counter, 0);
-#elif defined(STL_ATOMIX) || defined(MUTEX_ATOMIX)
 		counter = 0;
-#elif defined(BOOST_ATOMIX)
-		// handled in ctor init list
-#else
-		#error unimplemented
-#endif
 	}
 
 	int inc() {
-#if defined(LINUX_ATOMIX)
-		return atomic_inc_return(&counter);
-#elif defined(STL_ATOMIX) || defined(BOOST_ATOMIX)
 		return ++counter;
-#elif defined(MUTEX_ATOMIX)
-		CMutexLocker ml(mMutex);
-		return ++counter;
-#else	
-		#error unimplemented
-#endif
 	}
 
 	int dec() {
-#if defined(LINUX_ATOMIX)
-		return atomic_dec_return(&counter);
-#elif defined(STL_ATOMIX) || defined(BOOST_ATOMIX)
 		return --counter;
-#elif defined(MUTEX_ATOMIX)
-		CMutexLocker ml(mMutex);
-		return --counter;
-#else	
-		#error unimplemented
-#endif
 	}
 
 	int value() const {
-#if defined(LINUX_ATOMIX)
-		return atomic_read(&counter);
-#elif defined(STL_ATOMIX) || defined(BOOST_ATOMIX) || defined(MUTEX_ATOMIX)
 		return (int)counter;
-#endif
 	}
 
 private:
-#if defined(LINUX_ATOMIX)
-	atomic_t counter;
-#elif defined(STL_ATOMIX)
 	std::atomic<int> counter;
-#elif defined(BOOST_ATOMIX)
-	boost::detail::atomic_count counter;
-#elif defined(MUTEX_ATOMIX)
-	CMutex mMutex;
-	volatile int counter;
-#else
-	#error unimplemented
-#endif
-	
 };
 
 #endif
